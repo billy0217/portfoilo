4020902c0e76bfb0378edd35de9b850d3fa0831c
billy <billyh880217@gmail.com>
2022-08-02 23:32:08 +1200

    integrate with contentful api

--- src/Pages/Project/Project.js
+++ src/Pages/Project/Project.js
@@ -1,25 +1,108 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { FiArrowLeft } from 'react-icons/fi';
-import { Link } from 'react-router-dom';
+import { Link,useParams } from 'react-router-dom';
+import {ProjectQuery} from '../../contentfull/contentfull';
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer';
+import Image from '../../Components/Image/Image';
 
 const Project = () => {
+
+	let params = useParams();
+	const query = ProjectQuery(params.slug);
+
+	const [projectData, setProjectData ] = useState([]);
+
+	const [imgSet, setImgSet ] = useState([]);
+
+	useEffect(() => {
+		const project = [];
+		const imageList = [];
+		window
+			.fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_CONTENTFULL_ID}/`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					"Authorization" : `Bearer ${process.env.REACT_APP_CONTENTFULL_ACCESS_TOKEN}`,
+				},
+				body: JSON.stringify({ query }),
+			})
+			.then((response) => response.json())
+			.then(({ data, errors }) => {
+				if (errors) {
+					console.error(errors);
+				}
+
+				// Project list
+				const p = data.projectCollection.items;
+				
+				project.push({
+					name: p[0].name,
+					slug: p[0].slug,
+					client: p[0].client,
+					link: p[0].link,
+					role: p[0].role,
+					tech: p[0].tech,
+					workFrom: p[0].workFrom,
+					type: p[0].type,
+					webp: p[0].image.url+"?fm=webp",
+					src: p[0].image.url,
+					content: p[0].description.json,
+					images: p[0].imagesCollection.items,
+				});
+				
+
+				setProjectData(project);
+				
+				if(project[0].images.length > 0) {
+					var len = project[0].images.length;
+					for (var i = 0; i < len; i++) {
+						imageList.push({
+							key: i,
+							webp: project[0].images[i].url+"?fm=webp",
+							src: project[0].images[i].url
+						});
+					}
+					setImgSet(imageList);
+				}else{
+					setImgSet([]);
+				}
+
+			});
+			
+	},[query]);
+
+
 	return (
 		<div>
 			<section className="c-section">
 				<div className="c-project__info">
 					<div className="c-project__info-wrap">
-						<h2 className="c-section__heading c-project__info-name">Project Name</h2>
+						<h2 className="c-section__heading c-project__info-name">{projectData[0]?.name}</h2>
 						<div className="c-project__img-wrap c-project__info-main-img">
-
+							<Image img={projectData[0]}/>
 						</div>
-						<p className="c-project__info-role">Role</p>
+						<p className="c-project__info-role">{projectData[0]?.role}</p>
 						<div className="c-project__info-detail">
-							<p>Details</p>
-							<p><a href="https://codepen.io/billy0217/pens/public" target="_blank" rel="noreferrer">Link</a></p>
+							{documentToReactComponents(projectData[0]?.content)}
+							{
+								projectData[0]?.tech &&
+								<p><strong>Technologies :</strong> {projectData[0]?.tech}</p>
+							}
+							{projectData[0]?.link &&
+								<p><a href={projectData[0]?.link} target="_blank" rel="noreferrer">{projectData[0]?.link}</a></p>
+							}
 						</div>
-						<div className="c-project__img-wrap">
 
-						</div>
+						{
+							imgSet?.map((img) => {
+								return (
+									<div key={img.key} className="c-project__img-wrap">
+										<Image img={img}/>
+									</div>
+								)
+							})
+						}
+
 					</div>
 					<div className="c-project__back-top-list">
 						<Link 
--- src/Pages/Work/Work.js
+++ src/Pages/Work/Work.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Card from '../../Components/Card/Card';
 import { ProjectListQuery } from "../../contentfull/contentfull";
 
@@ -6,16 +6,49 @@ const Work = () => {
 	
 	const query = ProjectListQuery(process.env.REACT_APP_PROJECTLIST_ID);
 
-	
-	const projectData = {
-		
-		slug : "project",
-		name: "Project",
-		role: "Full Stack Developer",
-		company: "Freelance",
-		webp: "/project.webp",
-		src: "/project.jpg"
-	}
+	const [projectData, setProjectData ] = useState([]);
+
+	useEffect(() => {
+		const projectList = [];
+		window
+			.fetch(`https://graphql.contentful.com/content/v1/spaces/${process.env.REACT_APP_CONTENTFULL_ID}/`, {
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					"Authorization" : `Bearer ${process.env.REACT_APP_CONTENTFULL_ACCESS_TOKEN}`,
+				},
+				body: JSON.stringify({ query }),
+			})
+			.then((response) => response.json())
+			.then(({ data, errors }) => {
+				if (errors) {
+					console.error(errors);
+				}
+
+				// ID list
+				const IDs = data.projectList.projectsCollection.items;
+				// Project list
+				const projects = data.projectCollection.items;
+				// Sort 
+				const sortProjectList = IDs.map((i)=> projects.find((j) => j.sys.id === i.sys.id));
+
+				var len = sortProjectList.length;
+				for (var i = 0; i < len; i++) {
+					projectList.push({
+						id: sortProjectList[i].sys.id,
+						name: sortProjectList[i].name,
+						slug: sortProjectList[i].slug,
+						webp: sortProjectList[i].thumb.url+"?fm=webp",
+						src: sortProjectList[i].thumb.url,
+						role: sortProjectList[i].role,
+						company: sortProjectList[i].workFrom
+					});
+				}
+
+				setProjectData(projectList);
+				
+			});
+	},[query]);
 
 	return (
 		<div>
@@ -25,12 +58,16 @@ const Work = () => {
 						<h2 className="c-section__heading">Selected Works</h2>
 					</div>
 					<div className="c-projects__list">
-						<div className="c-proejct">
-							<Card project={projectData} />
-						</div>
-						<div className="c-proejct">
-							<Card project={projectData} />
-						</div>
+						{
+							projectData?.map((item) => {
+								return (
+								<div className="c-proejct" key={item.id}>
+									<Card project={item} />
+								</div>
+								)
+							})
+						}
+						
 					</div>
 				</div>
 				
